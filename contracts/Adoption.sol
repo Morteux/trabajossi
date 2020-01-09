c75a3fa03a26ff08145c1526a2a8193e02614d6c
Morteux <ignis.melendez@gmail.com>
2020-01-09 11:35:57 +0100

    Modificando adoption.sol

--- contracts/Adoption.sol
+++ contracts/Adoption.sol
@@ -1,6 +1,15 @@
 pragma solidity ^0.5.0;
 pragma experimental ABIEncoderV2;   //Esperimental es necesario para "File memory" (?)
 
+
+/*
+ARRAYS
+length
+push() devuelve el elemento del final
+push(x) inserta x al final
+pop() elimina el elemento del final
+*/
+
 contract Adoption {
 
     struct File {
@@ -9,24 +18,31 @@ contract Adoption {
         uint size;          //Tamaño
         uint downloads;     //Número de descargas
         address uploader;   //Propietario
-        bytes32 date; //El tipo no se si es correcto    //Fecha de subida
+        uint date; //El tipo no se si es correcto    //Fecha de subida
     }
 
     File[] public files;
-    uint size = 0;
+    //mapping(File => address) private uploaders;
 
     // Devuelve la información del archivo que se va a descargar
     function download(uint fileId) public returns (File memory)
     {
-        require(fileId >= 0 && fileId < size);
+        require(fileId >= 0 && fileId < files.length, "Error en el índice al acceder a los archivos");
 
-        files[fileId].uploader = msg.sender;
+        files[fileId].downloads++;
 
         return files[fileId];
     }
 
+    // Devuelve la información del archivo que se va a descargar
+    function upload(bytes32 name, bytes3 ext, uint size) public
+    {
+        //File newFile = new File(name, ext, size, 0, msg.sender, now);
+        //files.push(newFile);
+    }
+
     // Devuelve la información de todos los archivos
-    function getFiles(uint fileId) public returns (File[] memory)
+    function getFiles() public returns (File[] memory)
     {
         return files;
     }
