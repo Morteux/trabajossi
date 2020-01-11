0f2ba90f246d10601f3f62659bc2538dac984757
Morteux <ignis.melendez@gmail.com>
2020-01-11 15:25:41 +0100

    Arreglado currentProvider

--- src/web3.js
+++ src/web3.js
@@ -1,5 +1,9 @@
 import Web3 from 'web3';
 
-const web3 = new Web3(/*windows.*/Web3.currentProvider);
+const web3 = new Web3(window.web3.currentProvider);
+
+//Alternativa -> This is only for version 1.x of web3
+//const web3 = new Web3(Web3.givenProvider || "ws://localhost:8546");
+//console.log(web3.currentProvider.isMetaMask)
 
 export default web3;
